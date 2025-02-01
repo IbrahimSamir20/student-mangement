d11891843171467c588732bcf4326fb2a672fce1
Ibrahim Samir <ibrahimsamir.tec@gmail.com>
2025-02-02 01:44:36 +0200

    create class custom Tabel

---
+++ src/main/java/org/example/studentadminstator/components/CustomTable.java
@@ -0,0 +1,5 @@
+package org.example.studentadminstator.components;
+
+public class CustomTable {
+        
+}
--- src/main/java/org/example/studentadminstator/data/User.java
+++ src/main/java/org/example/studentadminstator/data/User.java
@@ -1,5 +1,7 @@
 package org.example.studentadminstator.data;
 
 public class User {
+          
         
+
 }
