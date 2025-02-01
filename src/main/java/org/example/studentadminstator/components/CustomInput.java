5ffd318d5e7ce7e99ae9e8365f317d837ecebc44
Ibrahim Samir <ibrahimsamir.tec@gmail.com>
2025-02-02 00:37:06 +0200

    update login form , and careate valdiation , event onCHANGE

--- src/main/java/org/example/studentadminstator/components/CustomInput.java
+++ src/main/java/org/example/studentadminstator/components/CustomInput.java
@@ -2,6 +2,8 @@ package org.example.studentadminstator.components;
 
 import org.example.studentadminstator.AppStyle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -13,14 +15,16 @@ public class CustomInput extends VBox {
         private TextField input ;
         private Label label ;
         private Text errorMessage;
+        private Boolean isValid=false;
 
-        public CustomInput(String placeholder, String labelText, String errorText){
+        public CustomInput(String placeholder, String labelText){
               
                 //declare var
                 this.input = new TextField();
                 this.input.setPromptText(placeholder);
                 this.label = new Label(labelText);
-                this.errorMessage = new Text(errorText);
+                this.errorMessage = new Text("");
+                // this.labelText = labelText;
         
                 //style
                 this.input.setPrefWidth(300);
@@ -32,13 +36,47 @@ public class CustomInput extends VBox {
                 this.getChildren().addAll(label, input, errorMessage);
                 this.setSpacing(5);
         
+                this.input.textProperty().addListener(new ChangeListener<String>() {
+                        @Override
+                        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                                System.out.println(newValue);
+                                if (newValue.trim().isEmpty()) {
+                                         setErrorMessage(labelText + " is required");
+                                         isValid=false;
+                                         if(labelText=="Password"){
+                                                isValid=false;
+                                                if(input.getText().length() < 6){
+
+                                                        setErrorMessage(labelText + "must be at least 6 characters");
+                                                        isValid=false;
+                                                }
+                                         }
+                                      
+                                } else {
+                                        clearErrorMessage();
+                                        isValid=true;
+                                }
+                        }
+                });
         }
 
-        public boolean isValid(){
-                if(this.input.getText().trim().equals("")){
-                        return false;
-                }else{
-                        return true;
-                }
+        
+        public void setErrorMessage(String message){
+                this.errorMessage.setText(message);
+                this.errorMessage.setVisible(true);
         }
+        
+        public void clearErrorMessage(){
+                this.errorMessage.setText("");
+                this.errorMessage.setVisible(false);
+        }
+        public String getInputValue(){
+                return input.getText().trim();
+        }
+        
+        public boolean getIsValid(){
+                return isValid;
+        }
+        
+        
 }
--- src/main/java/org/example/studentadminstator/pages/Login.java
+++ src/main/java/org/example/studentadminstator/pages/Login.java
@@ -3,12 +3,8 @@ import org.example.studentadminstator.AppStyle;
 import org.example.studentadminstator.components.CustomInput;
 
 import javafx.scene.layout.VBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
-import javafx.scene.control.Alert;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
@@ -16,19 +12,19 @@ import javafx.stage.Stage;
 import javafx.scene.layout.Background;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-//use latter for controller
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class Login extends VBox {
-    private String errorUsername = "";
-    private String errorPassword = "";
-    private Boolean isVisible = false;
     private GridPane grid = new GridPane();
-    private CustomInput usernameInput = new CustomInput("Enter your username", "Username", errorUsername);
-    private CustomInput passwordInput = new CustomInput("Enter Password","Password",errorPassword);
-    private Button loginButton = new Button();
+    
+    String errorUsername = "";
+    String errorPassword = "";
+    Boolean isVisible = false;
+
+    private CustomInput usernameInput = new CustomInput("Enter your username", "Username");
+    private CustomInput passwordInput = new CustomInput("Enter Password","Password");
+    private Button loginButton = new Button("submit");
 
     public Login(Stage primaryStage) {
         
@@ -45,23 +41,19 @@ public class Login extends VBox {
         loginButton.setFont(Font.font(AppStyle.font18.getFamily(), FontWeight.BOLD, AppStyle.font18.getSize()));
         loginButton.setPrefWidth(100);
         loginButton.setTextFill(AppStyle.textColor);
+
         grid.add(usernameInput, 0, 0, 1, 1);
         grid.add(passwordInput, 0, 1, 1, 1);
-        grid.add(loginButton, 0, 4, 1, 1);
+        grid.add(loginButton, 0, 2, 1, 1);
 
         EventHandler<ActionEvent> buttonEvent = e -> {
-            if(usernameInput.isValid() && passwordInput.isValid()){
-                //Should Go to the next page
-            }else {
-                errorUsername = "Username cannot be empty";
-                errorPassword = "Password cannot be empty";
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Empty Field");
-                alert.setHeaderText(null);
-                alert.setContentText("Please fill all the fields correctly");
-                alert.showAndWait();
-
-            }
+            Boolean usernameValid =  usernameInput.getIsValid();
+            Boolean passwordValid =  passwordInput.getIsValid();
+            System.out.println("Username valid: " + usernameValid.toString() + ", Password valid: " + passwordValid.toString());
+                // System.out.println("logined");
+                if(usernameValid && passwordValid){
+                  System.out.println("submitted successful"+ usernameInput.getInputValue());
+                }
         };
 
         loginButton.setOnAction(buttonEvent);
