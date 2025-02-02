b4ef403c77ae2ea5485398d44e241e6de737ecc3
EsraaNehad <esraanehad0303@gmail.com>
2025-02-02 02:52:29 +0200

    create button style

--- src/main/java/org/example/studentadminstator/components/CustomButton.java
+++ src/main/java/org/example/studentadminstator/components/CustomButton.java
@@ -1,5 +1,18 @@
 package org.example.studentadminstator.components;
 
 public class CustomButton {
-        
+   Button button;
+
+        button.setTextFill(AppStyle.textColor);
+        button.setFont(Font.font(AppStyle.font18.getFamily(), FontWeight.BOLD, AppStyle.font18.getSize()));
+        button.setPrefWidth(100);
+        button.setFill(AppStyle.colorBlack );
+
+
+      button.setOnMousePressed(e -> button.setScaleX(0.9));
+      button.setOnMousePressed(e -> button.setScaleY(0.9));
+      button.setOnMouseReleased(e -> button.setScaleX(1.0));
+      button.setOnMouseReleased(e -> button.setScaleY(1.0));
+
+
 }
