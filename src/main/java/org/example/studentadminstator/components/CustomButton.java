a48322eb61cf1e229b84e629d335ae667fc6a8cf
osama55585 <https://github.com/osama55585/osama_Progects.git>
2025-02-01 16:20:50 -0800

    add CustomButton

--- src/main/java/org/example/studentadminstator/components/CustomButton.java
+++ src/main/java/org/example/studentadminstator/components/CustomButton.java
@@ -1,5 +1,16 @@
 package org.example.studentadminstator.components;
 
-public class CustomButton {
-        
+public class CustomButton extends Vbox {
+        private Button;
+public CustomButtontom(EventHandler<ActionEvent> eventHandler,String lapel){
+
+        this.button=new Button(label);
+
+        this.button.setonAction(eventHandler);
+
+        this.getchildren().add(this.button);
+
+        }
+
+
 }
