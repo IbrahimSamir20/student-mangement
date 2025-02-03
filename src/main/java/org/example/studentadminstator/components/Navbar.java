971195c6b8f005e1e2a7ba63cce2ba0a1d44781d
osama55585 <https://github.com/osama55585/osama_Progects.git>
2025-02-02 17:13:25 -0800

    osama

--- src/main/java/org/example/studentadminstator/components/Navbar.java
+++ src/main/java/org/example/studentadminstator/components/Navbar.java
@@ -1,12 +1,33 @@
 package org.example.studentadminstator.components;
 
 import javafx.scene.layout.HBox;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Navbar extends HBox {
-        
-
-
-
+        @Override
+        public void start(Stage primaryStage) {
+            Button button = new Button("Edit profille");
+            Button button2 = new Button("Back");
+            HBox hbox = new HBox(button,button2);
+            hbox.setAlignment(Pos.TOP_RIGHT);
+            hbox.setSpacing(10);
+            hbox.setPadding(new Insets(20));
+            Scene scene = new Scene(hbox, 400, 400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        public static void main(String[] args) {
+            launch();
+        }
+    }
 
 
 }
