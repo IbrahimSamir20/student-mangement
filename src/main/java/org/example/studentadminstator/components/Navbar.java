2f81a7f4b4ffab60d260e0526d89f1f45bf0714e
EsraaNehad <esraanehad0303@gmail.com>
2025-02-03 04:46:18 +0200

    NavBar

--- src/main/java/org/example/studentadminstator/components/Navbar.java
+++ src/main/java/org/example/studentadminstator/components/Navbar.java
@@ -1,12 +1,19 @@
 package org.example.studentadminstator.components;
 
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class Navbar extends HBox {
-        
 
+    private String uername ;
+    Text text;
 
-
-
+    public Navbar(String uername) {
+        this.uername = uername;
+        this.text= new Text("Hello,");
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(text);
+    }
 
 }
