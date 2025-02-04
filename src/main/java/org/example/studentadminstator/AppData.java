682fa0c29404b7bf96807970fff0e2790c66faf2
AGaafar7 <a.gaafar.junior@gmail.com>
2025-02-04 14:10:56 +0200

    Fixing Type

--- src/main/java/org/example/studentadminstator/AppData.java
+++ src/main/java/org/example/studentadminstator/AppData.java
@@ -14,8 +14,8 @@ public class AppData {
         instructorDB = new InstructorDB<>();
         coursesDB = new CoursesDB<>();
         usersDB = new UsersDB<>();
-        usersDB.createUser(new User("ibrahim", "abdelrahman aql", "instructor"));
-        usersDB.createUser(new User("ibrahim", "ibrahim", "student"));
+        usersDB.createUser(new User("ibrahim", "abdelrahman aql", "Instructor"));
+        usersDB.createUser(new User("ibrahim", "ibrahim", "Student"));
 
     }
 
