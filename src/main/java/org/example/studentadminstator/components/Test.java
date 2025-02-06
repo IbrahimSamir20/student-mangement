8ced1c3c5e032e56e8027fd9233316ded975bbb7
Ibrahim Samir <ibrahimsamir.tec@gmail.com>
2025-02-06 15:14:23 +0200

    sava change for pull

--- src/main/java/org/example/studentadminstator/components/Test.java
+++ src/main/java/org/example/studentadminstator/components/Test.java
@@ -1,11 +1,22 @@
 package org.example.studentadminstator.components;
 
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class Test {
     String username;
     String password;
+    private VBox container;
     public Test(String username, String password){
         this.username=username;
         this.password=password;
+    }
+
+    public VBox getContainer(){
+        Text newText = new Text();
+        System.out.println(username);
+container.getChildren();
+        return container;
     }
 
     public void print (){
